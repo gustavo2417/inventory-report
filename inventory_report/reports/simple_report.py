6c931f530520df918b2adc5c973231600e35773f
Gustavo <gustavodarocha241@gmail.com>
2023-04-29 05:57:41 -0300

    feat: requisitos 1 e 2 completos

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,46 @@
+from datetime import date
+
+
 class SimpleReport:
-    pass
+    def get_oldest_manufacturing_date(list):
+        result = list[0]["data_de_fabricacao"]
+
+        for i in list:
+            if i["data_de_fabricacao"] < result:
+                result = i["data_de_fabricacao"]
+
+        return result
+
+    def get_nearest_expiration_date(list):
+        today = str(date.today())
+        new_list = []
+
+        for i in list:
+            if i["data_de_validade"] >= today:
+                new_list.append(i["data_de_validade"])
+
+        result = min(new_list)
+
+        return result
+
+    def get_company_with_more_products(list):
+        counter = {}
+
+        for i in list:
+            if i["nome_da_empresa"] not in counter:
+                counter[i["nome_da_empresa"]] = 0
+            counter[i["nome_da_empresa"]] += 1
+
+        return max(counter, key=counter.get)
+
+    @staticmethod
+    def generate(list):
+        oldest = SimpleReport.get_oldest_manufacturing_date(list)
+        expiration_date = SimpleReport.get_nearest_expiration_date(list)
+        more_products = SimpleReport.get_company_with_more_products(list)
+
+        return (
+            f"Data de fabricação mais antiga: { oldest }\n"
+            f"Data de validade mais próxima: { expiration_date }\n"
+            f"Empresa com mais produtos: { more_products }"
+        )
--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(
+        3,
+        'Pneu de carro',
+        'Mercado livre',
+        '2023-05-22',
+        '2050-05-22',
+        '8853059',
+        'sla'
+    )
+
+    assert new_product.id == 3
+    assert new_product.nome_do_produto == 'Pneu de carro'
+    assert new_product.nome_da_empresa == 'Mercado livre'
+    assert new_product.data_de_fabricacao == '2023-05-22'
+    assert new_product.data_de_validade == '2050-05-22'
+    assert new_product.numero_de_serie == '8853059'
+    assert new_product.instrucoes_de_armazenamento == 'sla'
